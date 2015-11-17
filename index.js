09db3e4c63e00a1acf527c41a7d9ce155765e36e
davidmarkclements <huperekchuno@googlemail.com>
2015-11-17 12:44:56 +0000

    add sync, standarize error

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 var which = require('which')
 
+var NOT_FOUND = 'could not find VBoxManage (or VboxManage)'
+
 module.exports = function (cb) {
   which('VBoxManage', function (err, path) {
-    if (err) return which('VboxManage', cb)
+    if (err) return which('VboxManage', function (err, path) {
+      if (err) return cb(Error(NOT_FOUND))
+      cb(null, path)
+    })
     cb(null, path)
   })
+}
+
+module.exports.sync = function () { 
+  var path
+  try { 
+    path = which.sync('VBoxManage') 
+  } catch (e) {
+    try { 
+      path = which.sync('VboxManage')  
+    } catch (e2) { 
+      throw Error(NOT_FOUND)
+    }
+  }
+  return path
 }
