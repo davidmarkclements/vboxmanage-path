ba534bcf32cc4e4fd3024b1f487ce22e834b6535
David Mark Clements <huperekchuno@googlemail.com>
2015-11-27 12:57:31 -0800

    support windows === double the code

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ var NOT_FOUND = 'could not find VBoxManage (or VboxManage)'
 module.exports = function (cb) {
   which('VBoxManage', function (err, path) {
     if (err) return which('VboxManage', function (err, path) {
-      if (err) return cb(Error(NOT_FOUND))
+      if (err) {
+        if (process.platform !== 'win32') {
+          return cb(Error(NOT_FOUND))
+        }
+
+        return win(cb)
+      }
       cb(null, path)
     })
     cb(null, path)
@@ -19,9 +25,67 @@ module.exports.sync = function () {
   } catch (e) {
     try { 
       path = which.sync('VboxManage')  
-    } catch (e2) { 
-      throw Error(NOT_FOUND)
+    } catch (e2) {
+      if (process.platform !== 'win32') {
+        throw Error(NOT_FOUND)
+      }
+      path = winSync()
     }
   }
+  if (!path) throw Error(NOT_FOUND)
   return path
+}
+
+function winSync() {
+  var spawn = require('spawn-sync')
+  var cmd
+  try {
+    cmd = which.sync('cmd')
+  } catch (e) {
+    cmd = which.sync('command')
+  }
+  var result = spawn(cmd, ['/k', 'dir', 'VBoxManage.exe', '/s', '/b'], {cwd: '\\'})
+  if (result.status) {
+    throw result.error
+  }
+  var path = (result.output + '').split('\r')[0].replace(/^,/,'')
+  try {
+    path = which.sync(path)
+  } catch (e) {
+    throw Error(NOT_FOUND)
+  }
+  return path
+}
+
+function win(cb) {
+  var spawn = require('child_process').spawn
+  which('cmd', function (err, cmd) {
+    if (err) {
+      return which('command', function (err, cmd) {
+        if (err) return cb(err)
+        locate(cmd, cb)
+      })
+    }
+    locate(cmd, cb)
+  })
+
+  function locate(cmd, cb) {
+    var child = spawn(cmd, ['/k', 'dir', 'VBoxManage.exe', '/s', '/b'], {cwd: '\\'})
+    var path 
+    child.stdout.once('data', function (output) {
+      path = (output + '').split('\r')[0].replace(/^,/,'')
+      which(path, function (err) {
+        if (err) return cb(NOT_FOUND) 
+        child.kill()
+      })
+    })
+
+    child.on('exit', function (code) {
+      if (code || !path) return cb(Error(NOT_FOUND)) 
+      cb(null, path)
+    })
+
+  }
+
+
 }
